142a08c1a04463c20fa21d5396caa20b46283212
Kai Wieland <post@kai.gs>
2025-07-25 00:14:58 +0200

    Refactor encryption methods to use runtime configuration for API credential key and add safe_decrypt function for improved error handling

--- lib/schedshare/crypto.ex
+++ lib/schedshare/crypto.ex
@@ -3,22 +3,40 @@ defmodule Schedshare.Crypto do
   Handles encryption and decryption of data.
   """
 
-  @key Application.compile_env!(:schedshare, :api_credential_encryption_key)
-  # The API requires the IV to be the same as the key.
-  # AES-128-CBC uses a 128-bit (16-byte) IV.
-  @iv @key
-
   def encrypt(data) do
+    key = Application.fetch_env!(:schedshare, :api_credential_encryption_key)
+    # The API requires the IV to be the same as the key.
+    # AES-128-CBC uses a 128-bit (16-byte) IV.
+    iv = key
+
     padded_data = pad(data)
-    cipher = :crypto.crypto_one_time(:aes_128_cbc, @key, @iv, padded_data, encrypt: true)
+    cipher = :crypto.crypto_one_time(:aes_128_cbc, key, iv, padded_data, encrypt: true)
     Base.encode64(cipher)
   end
 
   def decrypt(encoded_data) do
+    key = Application.fetch_env!(:schedshare, :api_credential_encryption_key)
+    # The API requires the IV to be the same as the key.
+    # AES-128-CBC uses a 128-bit (16-byte) IV.
+    iv = key
+
     with {:ok, data} <- Base.decode64(encoded_data) do
-      decrypted = :crypto.crypto_one_time(:aes_128_cbc, @key, @iv, data, encrypt: false)
+      decrypted = :crypto.crypto_one_time(:aes_128_cbc, key, iv, data, encrypt: false)
       unpad(decrypted)
     end
+  end
+
+  @doc """
+  Safely decrypts a password that might have been double-encrypted.
+  Returns the original password or an error if decryption fails.
+  """
+  def safe_decrypt(encoded_data) do
+    case decrypt(encoded_data) do
+      {:error, _reason} -> {:error, :decryption_failed}
+      decrypted -> {:ok, decrypted}
+    end
+  rescue
+    _ -> {:error, :decryption_failed}
   end
 
   # PKCS7 padding
